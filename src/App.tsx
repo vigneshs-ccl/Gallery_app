896d0cb5c0b3519c6088a78bf5b67952e3eded43
vigneshs-ccl <vigneshkumar.s@crystalcodelabs.com>
2025-08-25 10:49:22 +0530

    feat:created empty react typescript project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import AlbumsPage from "./components/AlbumsPage";
+
+function App() {
+  return (
+    <>
+      <AlbumsPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/AlbumsPage.tsx
@@ -0,0 +1,5 @@
+const AlbumsPage: React.FC = () => {
+  return <h1 className="text-red-500 ">Helllo world</h1>;
+};
+
+export default AlbumsPage;
